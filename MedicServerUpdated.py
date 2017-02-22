b2be0c45d5b87daee0d8937e101524cc794a979e
Avinash Jain <avinashkalpitjain@gmail.com>
2017-02-21 17:24:43 -0800

    Playing around

--- MedicServerUpdated.py
+++ MedicServerUpdated.py
@@ -4,6 +4,11 @@ from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
 # Try adding your own number to this list!
+account_sid = "ACa9eca256e7d2b82539a0c6086dc244d7"
+auth_token = "213a8dd83633246a86c5b36361665220"
+client = TwilioRestClient(account_sid, auth_token)
+
+
 callers = {
     "+14252298079": "Avinash Jain",
 }
@@ -12,16 +17,17 @@ callers = {
 def hello_monkey():
     """Respond and greet the caller by name."""
     body = request.values.get('Body', None)
-
+    media = request.values.get('MediaUrl0', None)
     from_number = request.values.get('From', None)
     if from_number in callers:
         message = callers[from_number] + ", thanks for the message! " + body
+
     else:
         message = "Monkey, thanks for the message!"
 
     resp = twilio.twiml.Response()
     resp.message(message)
-
+    resp.media(media)
     return str(resp)
 
 if __name__ == "__main__":
