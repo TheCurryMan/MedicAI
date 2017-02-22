f5cffdb1ce0dc04281cc5b39e8e609216fa5e660
Avinash Jain <avinashkalpitjain@gmail.com>
2017-02-21 16:44:29 -0800

    heroku server up

--- MedicServerUpdated.py
+++ MedicServerUpdated.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import twilio.twiml
 
 app = Flask(__name__)
 
+# Try adding your own number to this list!
+callers = {
+    "+14242298079": "Avinash Jain",
+}
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond and greet the caller by name."""
 
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
 
-if __name__ == '__main__':
-    app.run()
+    resp = twilio.twiml.Response()
+    resp.message(message)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
