3b6a5d0bf716ec53e2ef95e555c06ccbd8efeb9e
Avinash Jain <avinashkalpitjain@gmail.com>
2017-02-21 17:27:25 -0800

    Playing around

--- MedicServerUpdated.py
+++ MedicServerUpdated.py
@@ -20,14 +20,13 @@ def hello_monkey():
     media = request.values.get('MediaUrl0', None)
     from_number = request.values.get('From', None)
     if from_number in callers:
-        message = callers[from_number] + ", thanks for the message! " + body
+        message = callers[from_number] + ", thanks for the message! " + body + " " + media
 
     else:
         message = "Monkey, thanks for the message!"
 
     resp = twilio.twiml.Response()
     resp.message(message)
-    resp.media(media)
     return str(resp)
 
 if __name__ == "__main__":
