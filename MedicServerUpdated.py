028250a9516dfebab2659ab06052899e1ddcf6ea
Avinash Jain <avinashkalpitjain@gmail.com>
2017-02-21 17:05:26 -0800

    update

--- MedicServerUpdated.py
+++ MedicServerUpdated.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 import twilio.twiml
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
-
 # Try adding your own number to this list!
 callers = {
     "+14252298079": "Avinash Jain",
@@ -11,10 +11,11 @@ callers = {
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond and greet the caller by name."""
+    body = request.values.get('Body', None)
 
     from_number = request.values.get('From', None)
     if from_number in callers:
-        message = callers[from_number] + ", thanks for the message!"
+        message = callers[from_number] + ", thanks for the message! " + body
     else:
         message = "Monkey, thanks for the message!"
 
