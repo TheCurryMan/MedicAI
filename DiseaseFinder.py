43d9d6b501637cb678fa73d36e1cebe73b651fb0
Avinash Jain <avinashkalpitjain@gmail.com>
2017-02-27 08:25:07 -0800

    Added description to twilio return

--- DiseaseFinder.py
+++ DiseaseFinder.py
@@ -1,7 +1,6 @@
 import urllib2
 import json
 
-
 class UrlBuilder:
     def __init__(self, base):
         if base[-1] != '?':
@@ -37,7 +36,7 @@ def getPotentialDiseasesFromIds(ids):
     gender = "male"
     year_of_birth = "1999"
     # Need to generate new token every 24 hours. Gonna be a pain.
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im5pa2hpbC5hamphcmFwdUBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjEwMTMiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMTctMDEtMDEiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTQ4ODE4MDQ0NywibmJmIjoxNDg4MTczMjQ3fQ.No4lc2w4VuEhy4V3h1Q3Q00asfD_8fhORp8IlGvP7yw"
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im5pa2hpbC5hamphcmFwdUBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjEwMTMiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMTctMDEtMDEiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTQ4ODIxOTI4MCwibmJmIjoxNDg4MjEyMDgwfQ.nM4ToB-PLuRXaeafRFTExIMWnQ7ur8sBgWLK8uigVM4"
     language = "en-gb"
     symptoms = ids
 
@@ -51,12 +50,27 @@ def getPotentialDiseasesFromIds(ids):
     #print("Gender: " + gender.title())
     #print("Age: " + str(2016 - int(year_of_birth)))
     #print("Symptom ID(s): " + str(",".join(symptoms)))
-    #print(base.getURL())
+    print(base.getURL())
+
+
     req = urllib2.Request(base.getURL())
     data = urllib2.urlopen(req).read()
     respjson = json.loads(data.decode("utf-8"))
+    print(respjson)
     finalData = ""
+    counter = 0
+
     for i in range(0, len(respjson)):
+        if counter == 2:
+            break
         finalData += "Name of disease: " + str(respjson[i]['Issue']['ProfName']) + "\n"
         finalData += "Likelihood: " + str(round(int(respjson[i]['Issue']['Accuracy']), 3)) + "%\n"
+        print(respjson)
+        with open('details.json') as data_file:
+            data = json.load(data_file)
+            finalData += data[str(respjson[i]["Issue"]["ID"])]["TreatmentDescription"] + "\n"
+        counter += 1
+
     return finalData
+
+print(getPotentialDiseasesFromIds(["13", "9"]))
